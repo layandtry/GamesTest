3506b93d21ffc32c5ca74d411d3a808aaac65e22
layandtry <lay_and_try@163.com>
2018-06-19 17:34:14 +0800

    在游戏首页加入了轮播图

--- app/src/main/java/com/wlw_02_19_wangmiao/gamestest/FullscreenActivity.java
+++ app/src/main/java/com/wlw_02_19_wangmiao/gamestest/FullscreenActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -83,6 +87,10 @@ public class FullscreenActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * 游戏部分的代码
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,7 +101,148 @@ public class FullscreenActivity extends AppCompatActivity {
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        final int screenWidth = dm.widthPixels;
+        final int screenHeight = dm.heightPixels - 50;//获取屏幕高度
+        ImageView home = (ImageView) findViewById(R.id.BookImage);
+        final View.OnTouchListener child;
 
+        child = new View.OnTouchListener() {//对新创建的image进行监听
+
+            int lastX, lastY;// 记录移动的最后的位置
+            // 获取Action
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:// 按下
+                        lastX = (int) event.getRawX();//获取事件发生时的坐标
+                        lastY = (int) event.getRawY();
+                        break;
+                    /**
+                     * layout(l,t,r,b) l Left position, relative to parent t Top
+                     * position, relative to parent r Right position, relative to
+                     * parent b Bottom position, relative to parent
+                     */
+                    case MotionEvent.ACTION_MOVE:   // 移动中动态设置位置
+                        int dx = (int) event.getRawX() - lastX;
+                        int dy = (int) event.getRawY() - lastY;
+                        int left = v.getLeft() + dx;
+                        int top = v.getTop() + dy;
+                        int right = v.getRight() + dx;
+                        int bottom = v.getBottom() + dy;
+                        if (left < 0) {
+                            left = 0;
+                            right = left + v.getWidth();
+                        }
+                        if (right > screenWidth) {
+                            right = screenWidth;
+                            left = right - v.getWidth();
+                        }
+                        if (top < 0) {
+                            top = 0;
+                            bottom = top + v.getHeight();
+                        }
+                        if (bottom > screenHeight) {
+                            bottom = screenHeight;
+                            top = bottom - v.getHeight();
+                        }
+                        v.layout(left, top, right, bottom);//将移动后的图相在新位置显示出来
+                        Log.i("", "position:" + left + ", " + top + ", " + right
+                                + ", " + bottom);
+
+
+                        ((RelativeLayout.LayoutParams) (v.getLayoutParams())).leftMargin = left;
+
+                        ((RelativeLayout.LayoutParams) (v.getLayoutParams())).topMargin = top;
+
+                        lastX = (int) event.getRawX();
+                        lastY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return true;
+
+            }
+
+        };
+
+        home.setOnTouchListener(new View.OnTouchListener() {
+
+            int lastX, lastY;
+            ImageView image;
+            RelativeLayout.LayoutParams params;
+            int left;
+            int top;
+            int right;
+            int bottom;
+
+            public boolean onTouch(View v, MotionEvent event) {
+                int ea = event.getAction();
+                Log.i("TAG", "Touch:" + ea);
+                switch (ea) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        RelativeLayout parentcontener = (RelativeLayout) findViewById(R.id.parentcontent);
+
+                        lastX = (int) event.getRawX();
+                        lastY = (int) event.getRawY();
+                        ImageView parent = (ImageView) findViewById(R.id.BookImage);
+                        params = new RelativeLayout.LayoutParams(parent.getWidth(), parent
+                                .getHeight());
+                        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                        params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                        image = new ImageView(FullscreenActivity.this);
+
+                        image.setLayoutParams(params);
+                        parentcontener.addView(image);
+                        image.setImageResource(R.drawable.books);
+                        image.setOnTouchListener(child);
+
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int dx = (int) event.getRawX() - lastX;
+                        int dy = (int) event.getRawY() - lastY;
+                        left = image.getLeft() + dx;
+                        top = image.getTop() + dy;
+                        right = image.getRight() + dx;
+                        bottom = image.getBottom() + dy;
+                        if (left < 0) {
+                            left = 0;
+                            right = left + image.getWidth();
+                        }
+                        if (right > screenWidth) {
+                            right = screenWidth;
+                            left = right - image.getWidth();
+                        }
+                        if (top < 0) {
+                            top = 0;
+                            bottom = top + image.getHeight();
+                        }
+                        if (bottom > screenHeight) {
+                            bottom = screenHeight;
+                            top = bottom - image.getHeight();
+                        }
+                        image.layout(left, top, right, bottom);
+                        Log.i("", "position:" + left + ", " + top + ", " + right
+                                + ", " + bottom);
+                        lastX = (int) event.getRawX();
+                        lastY = (int) event.getRawY();
+                        params.leftMargin=left;
+                        params.topMargin=top;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // params.setMargins(left, top, right, bottom);加上这句话，运行的时候可能出现小bug，但不是
+
+                        //每次都出现，可测试一下
+
+                        break;
+                }
+                return true;
+            }
+        });
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
